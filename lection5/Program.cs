9e0b5445c0e1a33d4fec5087a0eb64e0a7cc08ea
mila <milavoronina1997@mail.ru>
2022-09-06 23:33:11 +0500

    lection5

--- lection5/Program.cs
+++ lection5/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] CreateArray(int N)
+{
+    int[] array = new int[N*2+1]; // 31
+    for(int i = -N; i <= N; i++) // -15 
+    {
+        array[i+N] = i;
+        Console.WriteLine(array[i]);
+        
+    }
+    return array;
+}
+
+int N = 15;
+int[] massiv = CreateArray(N);
